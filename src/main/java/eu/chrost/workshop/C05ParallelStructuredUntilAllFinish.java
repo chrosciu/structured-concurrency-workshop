a8c54493f9524d062eaae5ddeea21e75bf044f68
Marcin Chrost <marcin.chrost@allegro.com>
2025-05-27 21:39:47 +0200

    Added example of parallel structured tasks waiting untill all finish

---
+++ src/main/java/eu/chrost/workshop/C05ParallelStructuredUntilAllFinish.java
@@ -0,0 +1,24 @@
+package eu.chrost.workshop;
+
+import java.util.concurrent.StructuredTaskScope;
+
+class C05ParallelStructuredUntilAllFinish {
+    static <T, U>String runInParallelStructuredUntilAllFinish(Action<T> actionOne, Action<U> actionTwo) {
+        try (var scope = StructuredTaskScope.open(StructuredTaskScope.Joiner.awaitAll())) {
+            StructuredTaskScope.Subtask<T> subtaskOne = scope.fork(() -> actionOne.run());
+            StructuredTaskScope.Subtask<U> subtaskTwo = scope.fork(() -> actionTwo.run());
+            scope.join();
+            return String.join(",", getSubtaskResultAsString(subtaskOne), getSubtaskResultAsString(subtaskTwo));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getSubtaskResultAsString(StructuredTaskScope.Subtask<?> subtask) {
+        return switch (subtask.state()) {
+            case SUCCESS -> subtask.get().toString();
+            case FAILED -> subtask.exception().getMessage();
+            default -> throw new IllegalStateException("Unexpected subtask state: " + subtask.state());
+        };
+    }
+}
---
+++ src/test/java/eu/chrost/workshop/C05ParallelStructuredUntilAllFinishTest.java
@@ -0,0 +1,43 @@
+package eu.chrost.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.time.Duration;
+
+import static java.time.Duration.ofSeconds;
+import static org.awaitility.Awaitility.await;
+
+class C05ParallelStructuredUntilAllFinishTest extends BaseTest<String> {
+    @Test
+    @Timeout(5)
+    void actionsShouldBeRunInParallel() {
+        //given
+        Action<String> hotel = new Action<>("booking hotel", "hotel booked", ofSeconds(2));
+        Action<String> flight = new Action<>("booking flight", "flight booked", ofSeconds(3));
+
+        //when
+        getAsyncResult(() -> C05ParallelStructuredUntilAllFinish.runInParallelStructuredUntilAllFinish(hotel, flight));
+
+        //then
+        await().atLeast(Duration.ofMillis(2500)).atMost(Duration.ofMillis(3500)).untilAsserted(() -> {
+            assertResult("hotel booked,flight booked");
+        });
+    }
+
+    @Test
+    @Timeout(5)
+    void errorInOneActionShouldNotCancelRest() {
+        //given
+        Action<String> hotel = new Action<>("booking hotel", "hotel booked", ofSeconds(1), true);
+        Action<String> flight = new Action<>("booking flight", "flight booked", ofSeconds(2));
+
+        //when
+        getAsyncResult(() -> C05ParallelStructuredUntilAllFinish.runInParallelStructuredUntilAllFinish(hotel, flight));
+
+        //then
+        await().atLeast(Duration.ofMillis(1500)).atMost(Duration.ofMillis(2500)).untilAsserted(() -> {
+            assertResult("Action booking hotel failed,flight booked");
+        });
+    }
+}
