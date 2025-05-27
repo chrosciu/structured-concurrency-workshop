25ad163ad1d81fb52e20c0034f07ae914d23ef29
Marcin Chrost <marcin.chrost@allegro.com>
2025-05-27 22:21:42 +0200

    Custom joiner - returns all successful results and waits for all tasks regardless of their status

---
+++ src/main/java/eu/chrost/workshop/C06ParallelStructuredUntilAllFinishWithResults.java
@@ -0,0 +1,39 @@
+package eu.chrost.workshop;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.StructuredTaskScope;
+import java.util.stream.Stream;
+
+class UntilAllFinishWithSuccessfulResultsJoiner<T> implements StructuredTaskScope.Joiner<T, Stream<T>> {
+    Queue<T> results = new ConcurrentLinkedQueue<>();
+
+    @Override
+    public boolean onComplete(StructuredTaskScope.Subtask<? extends T> subtask) {
+        if (subtask.state() == StructuredTaskScope.Subtask.State.SUCCESS) {
+            results.offer(subtask.get());
+        }
+        return false;
+    }
+
+    @Override
+    public Stream<T> result() throws Throwable {
+        return results.stream();
+    }
+}
+
+class C06ParallelStructuredUntilAllFinishWithSuccessfulResults {
+    @SafeVarargs
+    static <T> List<T> run(Action<T>... actions) {
+        try (var scope = StructuredTaskScope.open(new UntilAllFinishWithSuccessfulResultsJoiner<T>())) {
+            Arrays.stream(actions)
+                    .forEach(action -> scope.fork(action::run));
+            var results = scope.join();
+            return results.toList();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/eu/chrost/workshop/C06Test.java
@@ -0,0 +1,44 @@
+package eu.chrost.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.time.Duration;
+import java.util.List;
+
+import static java.time.Duration.ofSeconds;
+import static org.awaitility.Awaitility.await;
+
+class C06Test extends BaseTest<List<String>> {
+    @Test
+    @Timeout(5)
+    void actionsShouldBeRunInParallel() {
+        //given
+        Action<String> hotel = new Action<>("booking hotel", "hotel booked", ofSeconds(2));
+        Action<String> flight = new Action<>("booking flight", "flight booked", ofSeconds(3));
+
+        //when
+        getAsyncResult(() -> C06ParallelStructuredUntilAllFinishWithSuccessfulResults.run(hotel, flight));
+
+        //then
+        await().atLeast(Duration.ofMillis(2500)).atMost(Duration.ofMillis(3500)).untilAsserted(() -> {
+            assertResult(List.of("hotel booked","flight booked"));
+        });
+    }
+
+    @Test
+    @Timeout(5)
+    void allSuccessfulResultsShouldBeReturnedAndRestIgnored() {
+        //given
+        Action<String> hotel = new Action<>("booking hotel", "hotel booked", ofSeconds(1), true);
+        Action<String> flight = new Action<>("booking flight", "flight booked", ofSeconds(2));
+
+        //when
+        getAsyncResult(() -> C06ParallelStructuredUntilAllFinishWithSuccessfulResults.run(hotel, flight));
+
+        //then
+        await().atLeast(Duration.ofMillis(1500)).atMost(Duration.ofMillis(2500)).untilAsserted(() -> {
+            assertResult(List.of("flight booked"));
+        });
+    }
+}
